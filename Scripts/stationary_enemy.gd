099a322f77a5c46ce4debaa14dbdf3759046cf9c
Shadow-Diamond <58434079+Shadow-Diamond@users.noreply.github.com>
2025-03-22 19:04:46 -0500

    More Stuff Done
    Need to figure out signals

--- Scripts/stationary_enemy.gd
+++ Scripts/stationary_enemy.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+signal hitPlayer
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.get_parent().get_name() == "Player":
+		print("Hit Player")
+		hitPlayer.emit()

f7287280bd5f52ae4831b68fe7e109f4029f098f
loholla <147402627+loholla@users.noreply.github.com>
2025-05-01 01:04:54 -0500

    Shardback Crawler

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -10,8 +10,14 @@ extends CharacterBody2D
 @export var mc_right_marg: float
 @export var mc_bottom_marg: float
 
+@onready var death_delay: Timer = $death_delay
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var respawn_delay: Timer = $respawn_delay
+
 var dead = false
 var health = 1
+var death_delay_passed: bool = false
+var player_death: bool = false
 
 signal hit_player
 signal kill_bounce
@@ -45,9 +51,17 @@ func _physics_process(delta: float) -> void:
 			velocity.x = Speed
 		else:
 			velocity.x = 0
-	else:
+	elif not death_delay_passed:
 		velocity.x = 0
 		velocity.y = 0
+	else:
+		if !player_death:
+			collision_shape_2d.queue_free()
+			player_death = true
+			velocity.y = -Jump_Velocity/kill_bounce_decrease
+			respawn_delay.start()
+		velocity += get_gravity() * delta
+		
 	
 	move_and_slide()
 
@@ -57,6 +71,7 @@ func _on_hit_player():
 		1:
 			dead = true
 			health -= 1
+			death_delay.start()
 		2:
 			health -= 1
 		3:
@@ -64,3 +79,11 @@ func _on_hit_player():
 
 func _on_kill_bounce():
 	velocity.y = -Jump_Velocity/kill_bounce_decrease
+
+
+func _on_death_delay_timeout() -> void:
+	death_delay_passed = true
+
+
+func _on_respawn_delay_timeout() -> void:
+	pass # Replace with function body.
--- Scripts/Virellia/shardback_crawler.gd
+++ Scripts/Virellia/shardback_crawler.gd
@@ -1,1 +1,27 @@
-extends Node
+extends CharacterBody2D
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player: CharacterBody2D = $"../Player"
+@onready var kill_box: Area2D = $kill_box
+
+@export var stationary: bool = true
+
+var delayed: bool = false
+
+func _ready() -> void:
+	sprite.play("Idle")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	move_and_slide()
+	
+	if !stationary:
+		pass
+
+func _on_kill_box_body_entered(body: CharacterBody2D) -> void:
+	if body == player:
+		print("Enemy damage player signal")
+		player.emit_signal("hit_player")
